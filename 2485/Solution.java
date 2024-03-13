25df4b399cc3b85e176acc56cd58abce75b2da47
Varadut1 <varaduttarwar535@gmail.com>
2024-03-13 07:25:51 +0530

    2485

---
+++ 2485/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int total = n*(n+1)/2;  
+        int sum = 0; 
+        for(int i = 1; i<=n; i++){
+            sum += i;
+            if((total-sum+i) == sum)
+                return i;
+        }
+        return -1;
+    }
+}
